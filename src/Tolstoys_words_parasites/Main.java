1bfff65c1b77666bb23d14949086c8e90f2177bc
Anton <antonziderer1@gmail.com>
2022-03-06 22:19:10 +0300

    Начало 3 лабы

---
+++ src/Tolstoys_words_parasites/Main.java
@@ -0,0 +1,12 @@
+package Tolstoys_words_parasites;
+
+//        Подсчет частоты появления слов во входном потоке. Дать возможность указывать минимальную/максимальную длину слова,
+//        участвующую в подсчёте частоты, для фильтрации предлогов и местоимений. Использовать Java8 Stream API.
+//        Файл для анализа https://drive.google.com/open?id=1YnRy5H8Emx4kyA1-lLZkNuY8LDTplulu
+//        * Выяснить самые популярные прилагательные в тексте.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
