ec3ab94185c40ad11a3dc2c96ede30b020e1a008
Anton <antonziderer1@gmail.com>
2022-03-07 18:28:32 +0300

    3 лаба без тестов

--- src/Tolstoys_words_parasites/Main.java
+++ src/Tolstoys_words_parasites/Main.java
@@ -14,6 +14,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toMap;
+
 public class Main {
     static String regexPunct = "(?<!\\S)\\p{Punct}+|\\p{Punct}+(?!\\S)";
 
@@ -21,32 +23,39 @@ public class Main {
 
         File file = new File("res/vim1.txt");
 
+        System.out.println((wordCount(file,5,10)));
+
+
         System.out.println(wordCount_adject(wordCount(file,5,10)));
 
     }
 
-    public static List<Map.Entry<String, Long>> wordCount(File file, int minSize, int maxSize) throws IOException {
-        return Files.lines(Paths.get(file.getAbsolutePath()))
+    public static Map<String, Long> wordCount(File file, int minSize, int maxSize) throws IOException {
+
+        return   Files.lines(Paths.get(file.getAbsolutePath()))
                 .map(s -> s = s.replaceAll(regexPunct,""))
                 .map(s->s.toLowerCase(Locale.ROOT))
                 .flatMap( s -> Stream.of(s.split(" ")))
                 .filter(e->e.length()>minSize)
                 .filter(e->e.length()<maxSize)
-                .collect(Collectors.toMap(Function.identity(),v->1L,Long::sum))
+                .collect(toMap(Function.identity(), v->1L,Long::sum))
                 .entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .collect(Collectors.toList());
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
-    public static List<Map.Entry<String, Long>> wordCount_adject(List<Map.Entry<String, Long>> map){
+    public static Map<String, Long> wordCount_adject(Map<String, Long> map){
 
         String[] adject = new String[]{ "ой", "ый","ая","яя","ую","юю","ые","ое","их","ый","им"};
         Set<String> adjectSet = Arrays.stream(adject).collect(Collectors.toSet());
 
-        return map.stream()
+        return map.entrySet().stream()
                 .filter(s->adjectSet.contains(s.getKey()
                         .substring(s.getKey().length()-2)))
-                //.filter(s->!s.getKey().startsWith("котор"))
-                .collect(Collectors.toList());
+                .filter(s->!s.getKey().startsWith("котор"))
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
     }
+
 }
--- src/Tolstoys_words_parasites/MainTest.java
+++ src/Tolstoys_words_parasites/MainTest.java
@@ -2,13 +2,23 @@ package Tolstoys_words_parasites;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static Tolstoys_words_parasites.Main.wordCount;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    File file = new File("res/test1.txt");
 
     @Test
-    void wordCountTest(){
+    void wordCountTest() throws IOException {
+        //List<Map.Entry<String, Long>> test1 = new List<Map.Entry<String, Long>>();
+        //test1.add("abc",3L);
 
+        //List<Map.Entry<String, Long>> abc = wordCount(file,3,5);
     }
 
 }
